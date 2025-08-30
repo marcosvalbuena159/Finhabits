7acaada6585fb94dbf5925f6779171f5166213c0
Picxis <jdrodriguezd28@gmail.com>
2025-08-29 18:58:23 -0600

    Añadir pagina a express

---
+++ Index.js
@@ -0,0 +1,28 @@
+// Importar express
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Servir archivos estáticos desde "Public"
+app.use(express.static('Public'));
+
+// Ruta principal
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "App", "View", "Index.html"));
+});
+
+//rutas adicionales
+app.get("/home", (req, res) => {
+  res.sendFile(path.join(__dirname, "App", "View", "home.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "App", "View", "Inicio_Sesion.html"));
+});
+
+// Levantar servidor
+app.listen(PORT, () => {
+  console.log('Servidor corriendo en http://localhost:3000');
+});
