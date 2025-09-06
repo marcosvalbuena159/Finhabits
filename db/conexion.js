1611a3c9afd624cb02bc589ac872b2bc91f1b820
marcosvalbuena159 <marcos.valbuena560@gmail.com>
2025-09-06 13:18:11 -0500

    octaba-carga

--- db/conexion.js
+++ db/conexion.js
@@ -7,16 +7,6 @@ const conexion = mysql.createConnection ({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
-})
-
-//Verificamos la conexion a la base de datos
-conexion.connect((err) => {
-    if(err){
-        console.err("Error al conectarse con la base de datos" + err);
-        return;
-    } else {
-        console.log("Conexion exitosa a la base de datos");
-    }
 });
 
 //exportamos la conexion
